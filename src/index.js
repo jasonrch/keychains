813687d4693302febf46d3286cdc921125277f17
jasonrch <151870228+jasonrch@users.noreply.github.com>
2020-09-20 02:21:10 -0500

    Set up complete

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './Redux/store';
+import {Provider} from 'react-redux';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
